2da7ff4a6857b20cf11305fa0f89d3998784a63b
Andreas Bigger <abigger87@gmail.com>
2021-09-15 13:27:29 -0700

    port assets

--- pages/index.js
+++ pages/index.js
@@ -153,7 +153,6 @@ function Home() {
       <Head>
         <title>Rustaceans</title>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.webmanifest" />
       </Head>
